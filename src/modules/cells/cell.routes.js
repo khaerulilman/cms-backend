de56d9da756f6fbc59025571df4757fcc6cfc276
khaerulilman <khaerulilman10@gmail.com>
2026-02-06 08:17:43 +0700

    cookie http only for token

--- src/modules/cells/cell.routes.js
+++ src/modules/cells/cell.routes.js
@@ -15,9 +15,6 @@ const controller = new CellController();
 
 // Configure multer for file uploads
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     cb(
