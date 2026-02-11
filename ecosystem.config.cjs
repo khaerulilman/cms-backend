ebc56b05f312c7d1928ea4ae60bc9d1c028cb089
khaerulilman <khaerulilman10@gmail.com>
2026-02-11 23:14:02 +0700

    fix ecosystem config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,19 +1,26 @@
 module.exports = {
   apps: [
+    // 🔴 PRODUCTION
     {
       name: "portfolio-cms",
       script: "src/server.js",
       exec_mode: "fork",
       instances: 1,
-
-      env: {
-        NODE_ENV: "development",
-        PORT: 4000,
-      },
-
       env_production: {
         NODE_ENV: "production",
         PORT: 4000,
+      },
+    },
+
+    // 🟡 STAGING
+    {
+      name: "portfolio-cms-staging",
+      script: "src/server.js",
+      exec_mode: "fork",
+      instances: 1,
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 4001,
       },
     },
   ],
