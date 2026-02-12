a33a2d501921d26b2398b01503914390a40c4a6c
khaerulilman <khaerulilman10@gmail.com>
2026-02-12 12:03:22 +0700

    add branch staging

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "portfolio-cms",
+      name: "portfolio-cms", // nama app tetap sama
       script: "src/server.js",
       exec_mode: "fork",
       instances: 1,
@@ -13,7 +13,12 @@ module.exports = {
 
       env_production: {
         NODE_ENV: "production",
-        PORT: 4000,
+        PORT: 4000, // production tetap di 4000
+      },
+
+      env_staging: {
+        NODE_ENV: "staging",
+        PORT: 4001, // staging di 4001
       },
     },
   ],
