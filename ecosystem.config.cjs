38a228bed95f8c1bf1e9af47d9caa3b2e0c94fc7
khaerulilman <khaerulilman10@gmail.com>
2026-02-12 16:00:43 +0700

    fix ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,11 +6,6 @@ module.exports = {
       exec_mode: "fork",
       instances: 1,
 
-      env: {
-        NODE_ENV: "development",
-        PORT: 4000,
-      },
-
       env_production: {
         NODE_ENV: "production",
         PORT: 4000,
